746005286f45fc7245a423882c7f8ee6a1541f11
alitinart <alitinart@gmail.com>
2023-08-09 10:13:53 +0200

    Day 2

---
+++ ch-9/src/A.java
@@ -0,0 +1,11 @@
+public class A {
+    private double radius = 1;
+    /** Find the area of this circle */
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+    public static void main(String[] args) {
+        A myCircle = new A();
+        System.out.println("Radius is " + myCircle.radius);
+    }
+}
